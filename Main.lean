ed75777dae0f334643dc103fc9e172d0436135bd
ART <anand.rao.art@gmail.com>
2023-02-09 07:30:10 +0000

    Add `Qq` dependency

--- Main.lean
+++ Main.lean
@@ -9,7 +9,8 @@ def main (args : List String) : IO Unit := do
   initSearchPath (← findSysroot) 
     (["build/lib", 
     "lake-packages/mathlib/build/lib/",  
-    "lake-packages/std/build/lib/", 
+    "lake-packages/std/build/lib/",
+    "lake-packages/Qq/build/lib/", 
     "lake-packages/aesop/build/lib/"] |>.map System.FilePath.mk)
   let [decl] ← pure args | IO.throwServerError "usage: lean-gym <decl>"
   let decl := parseName decl
