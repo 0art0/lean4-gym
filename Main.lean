5f43c0277ff8ce19741e272afa69447a34d53f8e
ART <anand.rao.art@gmail.com>
2023-02-09 07:23:29 +0000

    Update search path

--- Main.lean
+++ Main.lean
@@ -6,8 +6,12 @@ def parseName (n : String) : Lean.Name :=
   (n.splitOn ".").foldl Lean.Name.mkStr Lean.Name.anonymous
 
 def main (args : List String) : IO Unit := do
-  initSearchPath (← findSysroot)
-  let [decl] ← pure args | throw (IO.userError "usage: lean-gym <decl>")
+  initSearchPath (← findSysroot) 
+    (["build/lib", 
+    "lake-packages/mathlib/build/lib/",  
+    "lake-packages/std/build/lib/", 
+    "lake-packages/aesop/build/lib/"] |>.map System.FilePath.mk)
+  let [decl] ← pure args | IO.throwServerError "usage: lean-gym <decl>"
   let decl := parseName decl
   let problem : Gym.Problem := { decl := decl, currNamespace := decl.getRoot }
   Gym.replFor problem
