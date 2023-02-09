a7d7415c0e2be35c3f5ef789df5df4bb804af9ab
ART <anand.rao.art@gmail.com>
2023-02-09 06:22:26 +0000

    Reintroduce `parseName`

--- Main.lean
+++ Main.lean
@@ -2,6 +2,9 @@ import LeanGym
 
 open Lean Lean.Meta Lean.Elab Lean.Elab.Tactic
 
+def parseName (n : String) : Lean.Name :=
+  (n.splitOn ".").foldl Lean.Name.mkStr Lean.Name.anonymous
+
 def main (args : List String) : IO Unit := do
   initSearchPath (← findSysroot)
   let [decl] ← pure args | throw (IO.userError "usage: lean-gym <decl>")
