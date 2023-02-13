6bdb3f22cdb192d925770972556637558e417cfc
ART <anand.rao.art@gmail.com>
2023-02-13 05:43:48 +0000

    Parsing namespaces

--- Main.lean
+++ Main.lean
@@ -1,9 +1,15 @@
 import LeanGym
 
-open Lean Lean.Meta Lean.Elab Lean.Elab.Tactic
+open Lean Lean.Meta Lean.Elab Lean.Elab.Tactic Parser
 
 def parseName (n : String) : Lean.Name :=
   (n.splitOn ".").foldl Lean.Name.mkStr Lean.Name.anonymous
+
+def parseNamespaces (ns : String) : List Name :=
+  if ns.front = '[' ∧ ns.back = ']' then
+    let ns' := ns.extract (ns.next ⟨0⟩) (ns.prev ns.endPos)
+    ns'.split (· = ' ') |>.map parseName
+  else []
 
 def main (args : List String) : IO Unit := do
   initSearchPath (← findSysroot) 
@@ -12,7 +18,10 @@ def main (args : List String) : IO Unit := do
     -- "lake-packages/std/build/lib/",
     -- "lake-packages/Qq/build/lib/", 
     "lake-packages/aesop/build/lib/"] |>.map System.FilePath.mk)
-  let [decl] ← pure args | IO.throwServerError "usage: lean-gym <decl>"
-  let decl := parseName decl
-  let problem : Gym.Problem := { decl := decl, currNamespace := decl.getRoot }
+  let ((decl : Name), (ns : List OpenDecl)) ←
+    match args with
+      | [decl] => pure (parseName decl, [])
+      | [decl, ns] => pure (parseName decl, parseNamespaces ns |>.map (.simple · []))
+      | _ => IO.throwServerError "usage: \nlean-gym <decl>\nlean-gym <decl> [ns₁ ns₂ ... nsₖ]"
+  let problem : Gym.Problem := { decl := decl, currNamespace := decl.getRoot, openDecls := ns }
   Gym.replFor problem
