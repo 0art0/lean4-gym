b10ea4ad69fb68306229ba06e374f65a9a36b900
ART <anand.rao.art@gmail.com>
2023-02-13 05:46:18 +0000

    Add option for parsing namespaces from the command line

--- Main.lean
+++ Main.lean
@@ -6,10 +6,10 @@ def parseName (n : String) : Lean.Name :=
   (n.splitOn ".").foldl Lean.Name.mkStr Lean.Name.anonymous
 
 def parseNamespaces (ns : String) : List Name :=
-  if ns.front = '[' ∧ ns.back = ']' then
-    let ns' := ns.extract (ns.next ⟨0⟩) (ns.prev ns.endPos)
-    ns'.split (· = ' ') |>.map parseName
-  else []
+  if ns.front = '[' ∧ ns.back = ']' then -- check whether the string is enclosed within `[` ... `]`
+    let ns' := ns.extract (ns.next ⟨0⟩) (ns.prev ns.endPos) -- extract the portion within `[` ... `]`
+    ns'.split (· = ' ') |>.map parseName -- split by whitespace
+  else [] -- defaults to no namespaces
 
 def main (args : List String) : IO Unit := do
   initSearchPath (← findSysroot) 
@@ -22,6 +22,7 @@ def main (args : List String) : IO Unit := do
     match args with
       | [decl] => pure (parseName decl, [])
       | [decl, ns] => pure (parseName decl, parseNamespaces ns |>.map (.simple · []))
+      -- namespaces are accepted as a space-separated list of identifiers enclosed within `[` ... `]`.
       | _ => IO.throwServerError "usage: \nlean-gym <decl>\nlean-gym <decl> [ns₁ ns₂ ... nsₖ]"
   let problem : Gym.Problem := { decl := decl, currNamespace := decl.getRoot, openDecls := ns }
   Gym.replFor problem
