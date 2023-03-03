422cf0ca6bf7d92a154679c049e6d3de2333e510
Anand Rao <18333981+0art0@users.noreply.github.com>
2023-03-03 14:01:50 +0000

    Relocate functions for parsing

--- LeanGym.lean
+++ LeanGym.lean
@@ -2,15 +2,6 @@ import LeanGym.LeanGym
 
 open Lean Lean.Meta Lean.Elab Lean.Elab.Tactic Parser
 
-def parseName (n : String) : Lean.Name :=
-  (n.splitOn ".").foldl Lean.Name.mkStr Lean.Name.anonymous
-
-def parseNamespaces (ns : String) : List Name :=
-  if ns.front = '[' ∧ ns.back = ']' then -- check whether the string is enclosed within `[` ... `]`
-    let ns' := ns.extract (ns.next ⟨0⟩) (ns.prev ns.endPos) -- extract the portion within `[` ... `]`
-    ns'.split (· = ' ') |>.map parseName -- split by whitespace
-  else [] -- defaults to no namespaces
-
 def main (args : List String) : IO Unit := do
   initSearchPath (← findSysroot) 
     (["build/lib", 
--- LeanGym/Parsing.lean
+++ LeanGym/Parsing.lean
@@ -4,6 +4,17 @@ open Lean Lean.Elab
 
 set_option autoImplicit false
 
+def parseName (n : String) : Lean.Name :=
+  (n.splitOn ".").foldl Lean.Name.mkStr Lean.Name.anonymous
+
+def parseNamespaces (ns : String) : List Name :=
+  if ns.front = '[' ∧ ns.back = ']' then -- check whether the string is enclosed within `[` ... `]`
+    let ns' := ns.extract (ns.next ⟨0⟩) (ns.prev ns.endPos) -- extract the portion within `[` ... `]`
+    ns'.split (· = ' ') |>.map parseName -- split by whitespace
+  else [] -- defaults to no namespaces
+
+-- def parseCommand (cmd : String) : 
+
 declare_syntax_cat gym_command
 syntax tactic : gym_command
 syntax num ":" tactic : gym_command
