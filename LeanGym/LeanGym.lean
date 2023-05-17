3ca13d9fa4031a9be799ea669bc2c45356bfe50e
Anand Rao <18333981+0art0@users.noreply.github.com>
2023-05-17 11:29:22 +0530

    New notation in code

--- LeanGym/LeanGym.lean
+++ LeanGym/LeanGym.lean
@@ -25,9 +25,9 @@ to recompute each path on every action.
 Example (circular) run of `lean-gym Nat.add_comm`:
 
 {"goals": ["⊢ ∀ (n m : Nat), n + m = m + n"], "errors": [], "branchId": 0}
-> {"runTactic": [0, "intro n m"]}
+> intro n m
 {"goals": ["n m : Nat\n⊢ n + m = m + n"], "errors": [], "branchId": 1}
-> {"runTactic": [1, "rw [Nat.add_comm]"]}
+> rw [Nat.add_comm]
 {"goals": [], "errors": [], "branchId": 2}
 -/
 import Lean
